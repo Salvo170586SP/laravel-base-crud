53ba48b67202b199c08b06f2e7c020b7f48cff80
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-22 15:51:40 +0100

    added route resource comics

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+//rotta delle pagine dei comics
+Route::resource('comics', 'ComicController');
