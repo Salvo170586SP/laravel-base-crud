5829c6c0f4e872aca56d4d7f77c999caf99c9736
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-22 16:04:56 +0100

    added comics on page index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,6 +4,19 @@
 
 <div class="container">
     <h2>Qui la lista dei comics</h2>
+
+    <ul>
+        @foreach($comics as $comic)
+        <li> <h2>{{ $comic->title }}</h2></li>
+        <li><p>{{ $comic->description }}</p></li>
+        <li><img src="{{ $comic->thumb }}" alt="$comic->title"> </li>
+        <li><p>{{ $comic->series }}</p></li>
+        <li><span>{{ $comic->price }}</span></li>
+        <li><span>{{ $comic->sale_date }}</span></li>
+        <li><p>{{ $comic->type }}</p></li>
+        @endforeach
+    </ul>
+
 </div>
 
 @endsection
